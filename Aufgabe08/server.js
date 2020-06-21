309f14e0520a3618d806c708e2d8257722169540
Lorenzo <lorenzo.geyer@gmx.de>
2020-06-22 01:47:03 +0200

    Aufgabe 8 Update

--- Aufgabe08/server.js
+++ Aufgabe08/server.js
@@ -1,26 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.A08Server = void 0;
-const Http = require("http");
+const Http = require("http"); //Laden des Modules (Erweiterung) HTTP, um einen Server zu bauen. * = sämtliche Funktionalität laden
 var A08Server;
 (function (A08Server) {
-    console.log("Starting server");
-    let port = Number(process.env.PORT);
+    console.log("Starting server"); //Bedeutungsloses Schlüsselwort aufgrund von "import"
+    let port = Number(process.env.PORT); //Erstellung Variable für Port Nummer
     if (!port)
-        port = 8100;
-    let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+        port = 8100; //Falls port = 8100
+    let server = Http.createServer(); //Erstelle Variable server und erstelle server
+    server.addListener("request", handleRequest); //Erstelle EventListener der auf requests hört
+    server.addListener("listening", handleListen); //Erstelle EventListener der hört
+    server.listen(port); // Höre den Server 8100 ab
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); //macht einen Eintrag in die Konsole sobald sie aufgerufen wird (und alles funktioniert)
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
+        //sobald ein request von Client Seite eintrifft & und gibt eine Response
+        console.log("I hear voices!"); //Nachdem dass geschehen ist erscheint der Konsoleneintrag "I hear voices!"
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // Festlegung des Headers für die Response
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
+        _response.write(_request.url); //Schreiben er Antwort als request.url
+        _response.end(); //Antwort zu Ende
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
 //# sourceMappingURL=server.js.map
--- Aufgabe08/server.ts
+++ Aufgabe08/server.ts
@@ -1,28 +1,29 @@
-import * as Http from "http";
+import * as Http from "http"; //Laden des Modules (Erweiterung) HTTP, um einen Server zu bauen. * = sämtliche Funktionalität laden
 
-export namespace A08Server {
-  console.log("Starting server");
-  let port: number = Number(process.env.PORT);
+export namespace A08Server { //Bedeutungsloses Schlüsselwort aufgrund von "import"
+  console.log("Starting server"); //Bedeutungsloses Schlüsselwort aufgrund von "import"
+  let port: number = Number(process.env.PORT); //Erstellung Variable für Port Nummer
   if (!port)
-    port = 8100;
+    port = 8100; //Falls port = 8100
 
-  let server: Http.Server = Http.createServer();
-  server.addListener("request", handleRequest);
-  server.addListener("listening", handleListen);
-  server.listen(port);
+  let server: Http.Server = Http.createServer(); //Erstelle Variable server und erstelle server
+  server.addListener("request", handleRequest); //Erstelle EventListener der auf requests hört
+  server.addListener("listening", handleListen); //Erstelle EventListener der hört
+  server.listen(port);    // Höre den Server 8100 ab
 
-  function handleListen(): void {
-    console.log("Listening");
+  function handleListen(): void { //Die Funktion Listening hat keinen Rückgabewert und
+    console.log("Listening");      //macht einen Eintrag in die Konsole sobald sie aufgerufen wird (und alles funktioniert)
   }
 
-  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-    console.log("I hear voices!");
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {  //Die Funktion "handleRequest" wird aufgerufen
+  //sobald ein request von Client Seite eintrifft & und gibt eine Response
+    console.log("I hear voices!");  //Nachdem dass geschehen ist erscheint der Konsoleneintrag "I hear voices!"
 
-    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("content-type", "text/html; charset=utf-8");  // Festlegung des Headers für die Response
     _response.setHeader("Access-Control-Allow-Origin", "*");
 
-    _response.write(_request.url);
+    _response.write(_request.url);  //Schreiben er Antwort als request.url
 
-    _response.end();
+    _response.end(); //Antwort zu Ende
   }
 }
