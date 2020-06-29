04c82cc2e246b8375feb7b473cfb1866155850fb
Lorenzo <lorenzo.geyer@gmx.de>
2020-06-29 23:03:48 +0200

    Aufgabe 9

--- Aufgabe09/script.ts
+++ Aufgabe09/script.ts
@@ -1,44 +1,32 @@
 namespace Aufgabe09 {
 
     let formData: FormData;
-    let respSplitted: string[];
+    let responseSplitted: string[];
     let myButton: HTMLButtonElement = (<HTMLButtonElement>document.getElementById("Senden-HTML"));
     myButton.addEventListener("click", handleHtmlButton);
     let jsonBtn: HTMLButtonElement = (<HTMLButtonElement>document.getElementById("Senden-JSON"));
     jsonBtn.addEventListener("click", handleJsonButton);
-    
-    function addToURL(): string { //Url mit Forminhalt
+
+    function formDataToUrl(): string {
         formData = new FormData(document.forms[0]);
-        let url: string = "https://samuelgis2020.herokuapp.com/";
-        //let url: string = "http://127.0.0.1:8100/Aufgabe09/index.html";
+        let url: string = "https://gislorenzo.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
         return url;
     }
-
-    async function handleHtmlButton(): Promise<void> {
-        getHtmlResponse(await addToURL());
-    }
-
-    async function handleJsonButton(): Promise<void> {
-        getJsonResponse(await addToURL());
-    }
-
-    async function getHtmlResponse(_url: RequestInfo): Promise<void> {     //get html from server
-
+    async function handleJsonButton(): Promise<void> { getJsonResponse(await formDataToUrl()); }
+    async function handleHtmlButton(): Promise<void> { getHtmlResponse(await formDataToUrl()); }
+    async function getHtmlResponse(_url: RequestInfo): Promise<void> {
         let response: Response = await fetch(_url, { method: "get" });
-        let resp2: string = await response.text();
-        respSplitted = resp2.split("<split>");
-        (<HTMLElement>document.getElementById("response")).innerHTML = respSplitted[0];
+        let response2: string = await response.text();
+        responseSplitted = response2.split("<split>");
+        (<HTMLElement>document.getElementById("response")).innerHTML = responseSplitted[0];
     }
-
-    async function getJsonResponse(_url: RequestInfo): Promise<void> {    //get json from server
-
+    async function getJsonResponse(_url: RequestInfo): Promise<void> {
         let response: Response = await fetch(_url, { method: "get" });
-        let resp2: string = await response.text();
-        
-        respSplitted = resp2.split("<split>");
-        let textParsed: string = JSON.parse(respSplitted[1]);
+        let response2: string = await response.text();
+        responseSplitted = response2.split("<split>");
+        let textParsed: string = JSON.parse(responseSplitted[1]);
         console.log(textParsed);
     }
 }
