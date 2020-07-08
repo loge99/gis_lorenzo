99b01f2a054a1a5fed5756f25e292f17c00cb0e3
Lorenzo <lorenzo.geyer@gmx.de>
2020-07-08 11:44:26 +0200

    Aufgabe 11

---
+++ Aufgabe11/script.js
@@ -0,0 +1,32 @@
+"use strict";
+var Aufgabe09;
+(function (Aufgabe09) {
+    document.getElementById("Senden-HTML")?.addEventListener("click", printHtml);
+    document.getElementById("Senden-JSON")?.addEventListener("click", printJson);
+    async function printJson(_event) {
+        //_event (Funktionsparamter) "Event" Steht für einen Typ; heißt das FUnktion nur auf Event reagiert, z.B. klickt auf button 
+        let formData = new FormData(document.forms[0]); //Formular data in formData packen, man brauch formular nicht genauer benennen, es gibt nur eins
+        let query = new URLSearchParams(formData); //Variable für query string vom typ URLSearchParams (da kommt formdata rein)
+        let url = "https://gislorenzo.herokuapp.com";
+        url = url + "/json"; //denn es wurde json button geklickt und das besagt die aufgabenstellung
+        url = url + "?" + query.toString(); // Informationen in url werden abgetrennt durch fragezeichen, hier werden sie als string an url angefügt, sind normalerweise nur metadaten
+        //im query.tostring sind informationen, z.B. FormData
+        // die daten
+        let response = await fetch(url); // client wartet auf serverantwort von der website "url", await dass er wartet bis ne antwort kommt bevor er weitermacht
+        let responseWebsiteText = await response.json(); //ERstellt die variable antwort, damit sie in 
+        //der Konsole angezeigt werden kann als json string
+        console.log(responseWebsiteText); //und hier wird dann die json in der konsole ausgegeben
+    }
+    async function printHtml(_event) {
+        let formData = new FormData(document.forms[0]);
+        let query = new URLSearchParams(formData);
+        let url = "https://gislorenzo.herokuapp.com";
+        url = url + "/html";
+        url = url + "?" + query.toString();
+        let response = await fetch(url); //Holt die url, die der server wieder zurückschickt (unter button als html in dem fall)
+        let responsetext = await response.text(); //holt die reponse und führt funktion text aus
+        let serverantwort = document.getElementById("serverantwort"); // hier anders weil ich das ganze als textdatei auf der website erzeugen will
+        serverantwort.innerHTML = responsetext;
+    }
+})(Aufgabe09 || (Aufgabe09 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe11/server.ts
@@ -0,0 +1,49 @@
+import * as Http from "http"; //- Laden des Modules (Erweiterung) HTTP, um einen Server zu bauen. * = sämtliche Funktionalität laden
+import * as Url from "url";  //- Aufgabe des Url Moduls ist die Aufsplittung der URl in lesbare Teile. Muss importiert werden zur Verwendung
+import * as Mongo from "mongodb";
+
+export namespace Aufgabe11 { //- Bedeutungsloses Schlüsselwort aufgrund von "import"
+  console.log("Starte Server"); //- Print "Starte Server" in der Console
+  let port: number = Number(process.env.PORT); //- Hat die Maschine mir einen port zugeteilt? - suche nach port, variable
+  if (!port)
+    port = 8100; //- Wenn nicht dann weise den Port 8100 zu
+
+  let server: Http.Server = Http.createServer(); //- Erstelle Variable server und erstelle server in ihr, rufe funktion auf
+  server.addListener("request", handleRequest); //- der server horcht, wenn er eine request erhält, ruft er die funktion handleRequest auf - 
+  server.addListener("listening", handleListen); //Erstelle EventListener der hört
+  server.listen(port);    //- Höre auf die Port nummer und schau ob eine Nachricht reinkommt -
+
+  function handleListen(): void { //Die Funktion Listening hat keinen Rückgabewert und
+    console.log("Listening");      //macht einen Eintrag in die Konsole sobald sie aufgerufen wird (und alles funktioniert)
+  }
+
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {  //- Funktion Handle Request wird aufgerufen,
+    //Bei der Request(Anfrage) kommt ein Parameter vom Typ "IncomingMessage" herein und es wird eine Antwort zur Verüfung gestellt "ServerResponse",
+    //die aber noch leer ist
+    _response.setHeader("content-type", "text/html; charset=utf-8");  //- Wir füllen die Response jetzt, bis jetzt nur mit einem Text
+    _response.setHeader("Access-Control-Allow-Origin", "*"); //- im Header befinden sich Infos zur Nachricht, im Body die Nachricht selbst  
+
+    if (_request.url) { //- Falls eine Url mit unserem url request reinkommt, wird er bearbeitet: ... >
+      let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true); //- Die Url muss übersetzt/lesbar gemacht werden mit "UrlWithParsedQuery"
+      //der url.parse wandelt die informationen von let url in ein anderes formular um und der Parameter "true" macht ein assoziatives array draus,
+      //welches sich leichter lesen lässt
+      let pathVariable: string | null = url.pathname; //oder 0, ist ein benötigter zusatz zu pathname
+      if (pathVariable == "/html") {
+        for (let key in url.query) { //! über ein assoziatives array iterieren? mittleres video
+          _response.write(key + ": " + url.query[key] + "<br/>"); //Die Antwort vom Server (mit Formatierung hier)
+        }
+      }
+
+
+
+      if (pathVariable == "/json") { //Wenn auf json geklickt wurde, schicke es als Json
+        let jsonString: string = JSON.stringify(url.query); //- die url als json formatieren und mit 
+        _response.write(jsonString); //- In der Repsonse wird jetzt auch noch dieser Json string mitgeschickt
+      }
+      console.log(_request.url);  //Schreiben er Antwort als request.url, unwichtig
+
+      _response.end(); //Antwort zu Ende, wenn vergessen wird response nicht veschickt
+    }
+  }
+  
+}
